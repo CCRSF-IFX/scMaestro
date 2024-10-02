872723909b59eb2bcbc93731c6e30fa54d5cbcf1
xies4 <xies4@nih.gov>
2024-10-02 13:47:20 -0400

    Add rerun, dryrun and unlock subcommands

---
+++ lib/smk_arg.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+from .utils import get_config_val, get_smk_file
+import sys
+
+def smk_rerun_dryrun_unlock(cmd, subparser):
+    subcommands = [i for i in subparser.choices if i not in ['rerun', 'dryun', 'unlock']]
+    if not os.path.exists("config.py") or not os.path.isfile("config.py"):
+        sys.exit(f'File "config.py" not found, please make sure you have run one of'
+                 f' the subcomands :{", ".join(subcommands)}\n')
+    pipeline, fullanalysis = get_config_val("config.py")
+    smk_file = get_smk_file(pipeline, fullanalysis)
+    smk_output = subprocess.check_output(f'snakemake -s {smk_file} -np', shell=True)
+    if cmd == 'rerun':
+        ('CONFIG FILE:')
+        with open('config.py') as f:
+            print(f.read())
+            submit = input('Submit now? (y/n): ')
+            while submit.lower() not in ['y', 'n']:
+                submit = input('Please answer yes(y) or no(n): ')
+            if submit.lower() == 'y':
+                subprocess.check_output('sbatch submit.sh', shell=True)
+                sflog.info("Submitted snakemake rerun in directory")
+        if cmd == 'unlock':
+                subprocess.check_output(f'snakemake -s {smk_file} --unlock', shell=True)
