3beb8b9baed936df1c0e55360dede583ee8952a2
xies4 <xies4@nih.gov>
2024-10-09 13:48:46 -0400

    Improve argument validation logic in argparse_multi.py

--- lib/argparse_multi.py
+++ lib/argparse_multi.py
@@ -2,15 +2,15 @@ import pandas as pd
 
 def multi_conditional_required_flag(args, parser):
     # Read the content of the input file
-    if getattr(args, "vdj_ref", None): 
-        df = pd.read_csv(args.library_config)
-        # Check if the file contains 'VDJ'
-        if df['Type'].str.contains('VDJ').any():
-            if not getattr(args, "vdj_ref", None):
-                parser.error("argument --vdj_ref is required when input file contains 'VDJ' data")
-        capture_types = ['Antibody Capture', 'Multiplexing Capture']
+    df = pd.read_csv(args.library_config)
 
-        for capture_type in capture_types:
-            if df['Type'].str.contains(capture_type).any():
-                if not getattr(args, "feature", None):
-                    parser.error(f"argument --feature is required when input file contains '{capture_type}' data")
+    # Check if the file contains 'VDJ'
+    if df['Type'].str.contains('VDJ').any():
+        if not getattr(args, "vdj_ref", None):
+            parser.error(": Argument --vdj_ref is required when input file contains 'VDJ' data\n")
+
+    # Check for capture types and require the --feature argument if present
+    capture_types = ['Antibody Capture', 'Multiplexing Capture']
+    if any(df['Type'].str.contains(capture_type).any() for capture_type in capture_types):
+        if not getattr(args, "feature", None):
+            parser.error(": Argument --feature is required when input file contains 'Antibody Capture' or 'Multiplexing Capture' data\n")
