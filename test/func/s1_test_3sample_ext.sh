5f73dfeb30309f6e2f775022fdaded6b0cdb34e5
xies4 <xies4@nih.gov>
2024-10-09 13:53:21 -0400

    Update workflow path in s1_test_3sample_ext.sh

--- test/func/s1_test_3sample_ext.sh
+++ test/func/s1_test_3sample_ext.sh
@@ -1,6 +1,6 @@
 conda activate /mnt/ccrsf-ifx/Software/tools/conda_env4scwf
 module load singularity
 
-snakemake -s workflow/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_rna_default
-snakemake -s workflow/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_vdj_default
-snakemake -s workflow/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_multi_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_rna_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_vdj_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_multi_default
