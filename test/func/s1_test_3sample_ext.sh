386a878668070c29b4f01f5f31922d0289d8281b
xies4 <xies4@nih.gov>
2024-10-16 10:25:09 -0400

    Add support for Multiome and ATAC pipelines

--- test/func/s1_test_3sample_ext.sh
+++ test/func/s1_test_3sample_ext.sh
@@ -4,3 +4,5 @@ module load singularity
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_rna_default
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_vdj_default
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_multi_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_atac_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_multiome_default
