e8da111cbd877b091255c8e29e1a6a619e347042
xies4 <xies4@nih.gov>
2024-10-18 10:55:44 -0400

    Update testing script

--- test/func/s1_test_3sample_ext.sh
+++ test/func/s1_test_3sample_ext.sh
@@ -2,7 +2,9 @@ conda activate /mnt/ccrsf-ifx/Software/tools/conda_env4scwf
 module load singularity
 
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_rna_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_rna_fullanalysis
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_vdj_default
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_multi_default
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_atac_default
 snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_multiome_default
+snakemake -s workflow_ext/Snakefile4test_ext --configfile config4test_3sample.ext.yaml --forceall -j 1 test_sc_fixedrna_singleplex_default
